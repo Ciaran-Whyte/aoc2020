cc850954cf96b7a18a34d615be37eae4fe1dd9b0
Ciaran Whyte <ciaran.whyte@fireeye.com>
2020-12-02 11:37:26 +0000

    Day 1 and 2

---
+++ day1/part2.py
@@ -0,0 +1,24 @@
+def readInputs(file_name: str) -> list[int]:
+    with open(file_name) as f:
+        lineInput = f.readlines()
+    return [int(x) for x in lineInput]
+
+
+def do_we_have_a_twenty_twenty_pair(input_one: int, remaining_numbers: list[int]) -> (bool, int):
+    for x in remaining_numbers:
+        if x + input_one == 2020:
+            return True, x
+    return False, None
+
+
+# numbers=[1721, 979, 366, 299, 675, 1456]
+numbers=readInputs("/Users/ciaran.whyte/dev/aoc2020/day1/input.txt")
+
+for idx, num in enumerate(numbers):        
+    for idx_two, num_two in enumerate(numbers[idx:]):
+        match, paired_num = do_we_have_a_twenty_twenty_pair(num + num_two, numbers[idx_two:])
+        if match:
+            print(f"{num} + {num_two} + {paired_num} == 2020")
+            print(f"{num} * {num_two} * {paired_num} == {num * num_two * paired_num}")
+            
+
---
+++ day2/part1.py
@@ -0,0 +1,23 @@
+def readInputs(file_name: str) -> list[str]:
+    with open(file_name) as f:
+        lineInput = f.readlines()
+    return [x for x in lineInput]
+
+
+# lines = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+lines = readInputs("/Users/ciaran.whyte/dev/aoc2020/day2/input.txt")
+
+def is_password_valid(min_count: int, max_count: int, character: str, password: str) -> bool:
+    # print(f"{character} {password}")
+    # print(f"{min_count} <= {password.count(character)} <= {max_count}")
+    return min_count <= password.count(character) <= max_count
+
+
+valid_count=0
+for line in lines:
+    min_max , char, password = line.split()
+    min_count, max_count = [int(p) for p in min_max.split('-')]
+    if is_password_valid(min_count, max_count, char.replace(":",""), password):
+        valid_count+=1
+
+print(f"valid_count {valid_count}")
